99ed5078c3b80fdb5883c430db8ca522bfb9a9e6
alx <alx@dispostable.com>
2012-03-05 22:23:42 +0100

    lulz.

---
+++ C/strcmp.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX 20 
+
+bool stringcmp( char *s1 , char *s2 ) {
+
+	for ( int i = 0 ; i < strlen(s2) ; i++ ) {
+
+		if ( s1[i] == s2[i] ) continue ;
+
+		else return false ;
+
+	}
+
+	return true ;
+
+}
+
+int main ( int argc , char** argv ) {
+
+	char* s1 = (char*) malloc(MAX*sizeof(char*)) ;
+	char* s2 = (char*) malloc(MAX*sizeof(char*)) ;
+
+	printf("Inserisci la prima stringa : ") ;
+	scanf("%s",s1) ;
+
+	printf("Inserisci la seconda stringa : ") ;
+	scanf("%s",s2) ;
+
+	if ( stringcmp( s1 , s2 ) ) printf("SUCCESS!!\n") ;
+
+	else printf("FAILED...\n") ;
+
+	return 0 ;
+
+}
